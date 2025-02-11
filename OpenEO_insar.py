2bc801cf84c8e5479b402bb4c71d8c3e19aa08c9
Emile Sonneveld <emile.sonneveld@vito.be>
2025-02-11 15:25:55 +0100

    Cleanup and format with black.

--- OpenEO_insar.py
+++ OpenEO_insar.py
@@ -21,32 +21,30 @@ else:
             "west": 10.751,
             "south": 46.741,
             "east": 10.759,
-            "north": 46.749
+            "north": 46.749,
         },
-        "temporal_extent": [
-            "2024-08-14",
-            "2024-08-26"
-        ]
+        "temporal_extent": ["2024-08-14", "2024-08-26"],
     }
 print(input_dict)
-print("AWS_ACCESS_KEY_ID= " + str(os.environ.get("AWS_ACCESS_KEY_ID", None)))  # Don't print AWS_SECRET_ACCESS_KEY
+print("AWS_ACCESS_KEY_ID= " + str(os.environ.get("AWS_ACCESS_KEY_ID", None)))
 
+center_point = (
+    (input_dict["spatial_extent"]["west"] + input_dict["spatial_extent"]["east"]) / 2,
+    (input_dict["spatial_extent"]["south"] + input_dict["spatial_extent"]["north"]) / 2,
+)
 argument_list = [
     "-s", input_dict["temporal_extent"][0],
     "-e", input_dict["temporal_extent"][1],
     # TODO: Specify spatial extent instead of point
-    "-x", str((input_dict["spatial_extent"]["west"] + input_dict["spatial_extent"]["east"]) / 2),
-    "-y", str((input_dict["spatial_extent"]["south"] + input_dict["spatial_extent"]["north"]) / 2),
+    "-x", str(center_point[0]),
+    "-y", str(center_point[1]),
     "-p", "vv",
-    "-S", "IW3"  # TODO: Dynamically determine sub-swat
+    "-S", "IW3",  # TODO: Dynamically determine sub-swat
 ]
 containing_folder = Path(os.path.dirname(__file__))
 
-# result_folder = containing_folder / "result"
-# result_folder.mkdir(parents=True, exist_ok=True)
-# result_folder = Path("/home/ubuntu")
 result_folder = Path.home()
-# tmp_insar = Path("/tmp/insar")
+# tmp_insar = Path("/tmp/insar") # TODO: No need to write tmp results to mount
 # tmp_insar.mkdir(parents=True, exist_ok=True)
 tmp_insar = result_folder
 
@@ -62,8 +60,7 @@ if "-o" in argument_list:
 else:
     argument_list = ["-o", str(tmp_insar)] + argument_list
 
-# assert not any(tmp_insar.iterdir())
-# cmd = [str(containing_folder /"utilities/sentinel1_burst_extractor_spatiotemporal.sh")] + argument_list
+# Allow for relative imports:
 os.environ["PATH"] = os.environ["PATH"] + ":" + str(containing_folder / "utilities")
 cmd = ["sentinel1_burst_extractor_spatiotemporal.sh"] + argument_list
 print(cmd)
@@ -77,23 +74,13 @@ bursts = glob.glob(glob_str)
 if len(bursts) == 0:
     raise Exception("No files found with glob: " + glob_str)
 
-input1 = bursts[0]
-input2 = bursts[1]
-# bursts_relative = [str(Path(burst).relative_to(tmp_insar)) for burst in bursts]
-# iw_component = bursts_relative[0].split("_")[4]
-# input1 = tmp_insar / "S1A_SLC_20240814T171550_030345_IW3_VV_441043.SAFE/manifest.safe"
-# input2 = tmp_insar / "S1A_SLC_20240826T171550_030345_IW3_VV_442708.SAFE/manifest.safe"
-# input1 = tmp_insar / "S1A_SLC_20240814T171550_030345_IW3_VV_042880.SAFE/manifest.safe"
-# input2 = tmp_insar / "S1A_SLC_20240826T171550_030345_IW3_VV_043168.SAFE/manifest.safe"
-# ('-Pinput1=/home/ubuntu/S1A_SLC_20240814T171550_030345_IW3_VV_441043.SAFE/manifest.safe',
-#  '-Pinput2=/home/ubuntu/S1A_SLC_20240825T052728_359500_IW2_VV_442501.SAFE/manifest.safe',)
-
 gpt_cmd = [
     "gpt",
     "/src/graphs/pre-processing_stackOverview_2images_GeoTiff.xml",
-    f"-Pinput1={input1}", f"-Pinput2={input2}",
+    f"-Pinput1={bursts[0]}",
+    f"-Pinput2={bursts[1]}",
     f"-PstackOverview_filename={result_folder}/stackOverview_2images.json",
-    f"-PcoregisteredStack_filename={result_folder}/Orb_Stack_2images"
+    f"-PcoregisteredStack_filename={result_folder}/Orb_Stack_2images",
 ]
 print(gpt_cmd)
 subprocess.run(gpt_cmd, check=True, stderr=subprocess.STDOUT)
