47c35d63e362b2c2a6c4b0e6807ebb0089fd559f
Emile Sonneveld <emile.sonneveld@vito.be>
2025-09-16 15:27:25 +0200

    Add simple unit test.

---
+++ tests/test_cwl.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import yaml
+
+repository_root = Path(__file__).parent.parent
+
+
+def test_cwl_syntax():
+    cwl_files = list((repository_root / "cwl").glob("*.cwl"))
+    assert cwl_files
+    for cwl_file in cwl_files:
+        print(f"Checking {cwl_file}")
+        yaml_parsed = list(yaml.safe_load_all(cwl_file.read_text()))
+        assert len(yaml_parsed) >= 1
