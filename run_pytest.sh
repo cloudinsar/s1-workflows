3ec60ec90922b22c43e99b6684bffcd4442c098d
Emile Sonneveld <emile.sonneveld@vito.be>
2025-09-23 11:18:33 +0200

    Pin python3.11. Include geopyspark for better tests.

--- run_pytest.sh
+++ run_pytest.sh
@@ -5,12 +5,6 @@
 
 # get containing folder:
 cd "$(dirname "$0")"
-# TODO: Make get-jars available as package script, so not the whole repo needs to be downloaded
-#git clone --recursive https://github.com/Open-EO/openeo-geopyspark-driver --depth 1
-cd openeo-geopyspark-driver
-python scripts/get-jars.py --force-download jars
-python -m pip install -e ".[dev]"
-cd ..
 apt-get install -y libkrb5-dev  # Avoid: "Command 'krb5-config --libs gssapi' returned non-zero exit status 127."
 python -m pip install -e ".[dev]"
 python -m pytest
