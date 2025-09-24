938c824c93a62bc39f4f1d40a56f31fc8cc237bf
Emile Sonneveld <emile.sonneveld@vito.be>
2025-09-24 11:57:43 +0200

    Make docker image smaller. 4.5GB-> 2.9Gb, to avoid straining the registery.

--- run_pytest.sh
+++ run_pytest.sh
@@ -4,6 +4,24 @@
 . /opt/venv/bin/activate
 
 # get containing folder:
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit
+
+cd /opt || exit
+# TODO: Checkout specific commit
+git clone --recursive https://github.com/Open-EO/openeo-geopyspark-driver --depth 1
+cd /opt/openeo-geopyspark-driver || exit
+git pull && git submodule update --init --recursive
+
+# jep is difficult to install and is not needed, so disable:
+sed -i '/jep==/ s/^/# /' setup.py
+
+# first pip install could fail, so run twice:
+python3 -m pip install -e . --extra-index-url https://artifactory.vgt.vito.be/api/pypi/python-openeo/simple || echo "Command failed, but ignored"
+python3 -m pip install -e . --extra-index-url https://artifactory.vgt.vito.be/api/pypi/python-openeo/simple
+
+# TODO: Make get-jars available as package script, so not the whole repo needs to be downloaded
+python3 scripts/get-jars.py --force-download jars
+
+# General dependencies are probably already installed. Now download test dependencies:
 python -m pip install -e ".[dev]"
 python -m pytest
