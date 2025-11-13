d90a278ab00886b5597aeb0e9520f2141dc27009
Emile Sonneveld <emile.sonneveld@vito.be>
2025-11-13 19:35:09 +0100

    remove another skip too

--- tests/test_cwl.py
+++ tests/test_cwl.py
@@ -17,8 +17,4 @@ def test_cwl_validate():
     assert cwl_files
     for cwl_file in cwl_files:
         print(f"Checking {cwl_file}")
-        if "sar_coherence.cwl" in str(cwl_file):
-            print("Skipping to avoid known error on CI.")
-            # cwl_utils.errors.JavascriptException: NodeJSEngine requires Node.js engine to evaluate and validate Javascript expressions, but couldn't find it.  Tried nodejs, node, docker run node:alpine"
-            continue
         exec_proc(["cwltool", "--disable-color", "--debug", "--validate", str(cwl_file)])
