82f30311807afde6d229f466db32a236a2816d4f
Emile Sonneveld <emile.sonneveld@vito.be>
2026-01-27 16:46:56 +0100

    Move del statements.

--- sar/utils/simple_stac_builder.py
+++ sar/utils/simple_stac_builder.py
@@ -168,9 +168,6 @@ def generate_catalog(
             crs_set.add(gdalinfo_stac["proj:epsg"])
             del gdalinfo_stac["proj:projjson"]  # remove verbose information
             del gdalinfo_stac["proj:wkt2"]  # remove verbose information
-            del gdalinfo_stac["proj:epsg"]  # remove to avoid empty results when dryrun is called with "dummy"
-            del gdalinfo_stac["proj:bbox"]  # remove to avoid empty results when dryrun is called with "dummy"
-            del gdalinfo_stac["proj:transform"]  # remove to avoid empty results when dryrun is called with "dummy"
             # keep "proj:epsg", otherwise UTM is assumed
             # if "proj:transform" in gdalinfo_stac:
             #     # might mess up x/y resolution, so remove
@@ -206,6 +203,9 @@ def generate_catalog(
                 gdalinfo_stac["proj:bbox"] = latlon_bbox
             else:
                 gdalinfo_stac["proj:bbox"] = native_bbox
+            del gdalinfo_stac["proj:epsg"]  # remove to avoid empty results when dryrun is called with "dummy"
+            del gdalinfo_stac["proj:bbox"]  # remove to avoid empty results when dryrun is called with "dummy"
+            del gdalinfo_stac["proj:transform"]  # remove to avoid empty results when dryrun is called with "dummy"
 
             stac["bbox"] = latlon_bbox
             collection_stac["extent"]["spatial"]["bbox"][0] = union_aabbox(
