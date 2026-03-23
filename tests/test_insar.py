29d0ef9b0f70a77ace032c5aaf9e7b4cd79390ea
Emile Sonneveld <emile.sonneveld@vito.be>
2026-03-23 18:02:48 +0100

    input_dict_capetown_new.json

--- tests/test_insar.py
+++ tests/test_insar.py
@@ -85,6 +85,7 @@ def run_stac_catalog_and_verify(catalog_path: Path, tmp_dir: Path):
         # "sar/example_inputs/input_dict_2024_vv_new.json",
         # "sar/example_inputs/input_dict_2018_vh_new_spatial_extent.json",
         "sar/example_inputs/input_dict_andes_new.json",  # nodata
+        "sar/example_inputs/input_dict_capetown_new.json",  # ok
         "sar/example_inputs/input_dict_georgia_new.json",  # ok
         # "sar/example_inputs/input_dict_japan_new.json",  # tags missing
         "sar/example_inputs/input_dict_lapaz_new.json",  # nodata
